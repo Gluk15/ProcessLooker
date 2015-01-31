971e97e8c6cb3e21ef193a34a19780d5c0948e0b
Roman Intore <gluk1505@gmail.com>
2015-01-31 18:40:52 +0300

    Клиентская часть для получения запущенных приложений, без обновления

---
+++ ProcessLooker/ProccesLooker/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProccesLooker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int update = 1000;
+            List<Procc> listProcc = new List<Procc>();
+            Process[] procList = Process.GetProcesses();
+            foreach (Process pr in procList)
+            {
+                if (pr.MainWindowTitle!="")
+                {
+                    listProcc.Add(new Procc(pr.Id, pr.MainWindowTitle, pr.StartTime));
+                }
+            }
+ 
+		 listBox1.DataSource = listProcc;
+
+            
+
+        }
+    }
+}
---
+++ ProcessLooker/ProccesLooker/Procc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProccesLooker 
+{
+    class Procc
+    {
+        private int id;
+        private string name;
+        private DateTime start;
+        private DateTime end;
+
+        public Procc(int id, string name, DateTime start)
+        {
+            this.id = id;
+            this.name = name;
+            this.start = start;
+        }
+
+        public void Update(TimeSpan idle){
+            this.end = this.start + idle;
+        }
+
+        public override string ToString()
+        {
+            if (end != null) 
+            {
+                return String.Format("{0} || {1} || {2} || {3} ", id, name, start, end);
+            }
+            else
+            {
+                return String.Format("{0} || {1} || {2} ", id, name, start);
+            }
+        }
+
+    }
+}
